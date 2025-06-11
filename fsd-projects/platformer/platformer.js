4e9e3b8ec90d30330610bc92f25581b9677e12d7
fabian790 <fa788525@gmail.com>
2025-06-11 15:44:32 +0000

    completed bouncing box project

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -33,33 +33,44 @@ $(function () {
      //TODO 2 - Create Platforms
     createPlatform(100,640, 175,10,"pink" );
     createPlatform(400,625,20,200,"white");
+
     createPlatform(550,600,200,10,"pink" );
     createPlatform(750,600, 20,200 ," pink");
+
     createPlatform(750,500,20,100, " white");
     createPlatform(200,475,200,10, "pink");
+
     createPlatform(650,400,200,10 ," white");
     createPlatform(100,340,289,10," pink");
+
     createPlatform(330,220,150,10," white");
     createPlatform(475,50,10,200,"pink");
+
     createPlatform(1100,400,275,10,"white");
     createPlatform(850,600,300,10,"pink");
+
     createPlatform(1210,650,10,150,"white");
     createPlatform(700,275,275,10,"pink");
+
     createPlatform(900,420,150,10,"white");
     createPlatform(1100,150,200,10,"pink");
+
     createPlatform(1300,0,10,150 ,"white");
     createPlatform(100,640,10,200,"pink");
 
     // TODO 3 - Create Collectables
     createCollectable("database",450,200,1,0);
     createCollectable("database",100,700,1,0);
+
     createCollectable("database",700,700,1,0);
     createCollectable("database",1200,700,1,0);
-    createCollectable("database",1250,100,1,0)
+
+    createCollectable("database",1250,100,1,0);
     
     // TODO 4 - Create Cannons
-    createCannon("right",300,1000)
+    createCannon("right",300,1000);
     createCannon("top",600,1150);
+    
     createCannon("top",1000,1150 );
     createCannon("left",500,2000);
 
